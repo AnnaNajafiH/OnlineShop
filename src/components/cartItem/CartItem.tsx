5fcd6588a7ede2d3a7b6d7c56bc49989d069e361
Anna N <nahid.najafi.h@gmail.com>
2025-02-16 21:49:56 +0100

    ggg

--- src/components/cartItem/CartItem.tsx
+++ src/components/cartItem/CartItem.tsx
@@ -4,6 +4,7 @@ import { getProduct } from '../../services/api';
 import { IProduct } from '../../types/servers';
 import { useShoppingCartContext } from '../../context/ShoppingCartContext';
 import { Link } from 'react-router-dom';
+import { FaTrash } from 'react-icons/fa';
 
 interface ICartItem {
     id: number;
@@ -30,13 +31,23 @@ function CartItem({id,qty}:ICartItem ){
         className="rounded w-20"
         src={product?.image} alt="" />
         </Link>
-        <div className='ml-9'>
+        <div className='ml-8'>
             <h3 className="m-5">{product?.title}</h3>
-            <div className='mt-2 ml-5'>
-            <Button onClick={()=>handleIncreaseProductQty(id)} variant='primary'>+</Button>
+            <div className=' flex items-center gap-9 ml-6'>
+            <div className="flex gap-2 items-center">
+              <Button onClick={()=>handleIncreaseProductQty(id)} 
+            className="px-4 py-2 bg-blue-800 hover:bg-blue-600 text-white rounded-md">+
+            </Button>
             <span className='mx-2'>{qty}</span>
-            <Button onClick={()=>handleDecreaseProductQty(id)} variant='primary'>-</Button>
-            <Button onClick={()=>handleRemoveProduct(id)} className='mx-2' variant='danger'>Remove</Button>
+            <Button onClick={()=>handleDecreaseProductQty(id)} 
+            className="px-4 py-2 bg-blue-800 hover:bg-blue-600 text-white rounded-md"
+            >-</Button>
+            </div>
+
+            <Button onClick={()=>handleRemoveProduct(id)} 
+            className=" py-3 bg-red-700 hover:bg-red-500 text-white rounded-md ml-4">
+            <FaTrash style={{ fontSize: '24px', color: 'white' }} /> 
+            </Button>
             </div>
         </div>
 
